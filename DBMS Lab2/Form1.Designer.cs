e4ff7695f97ae412de69bd85c1778362c633b6db
ColdCipri <colder.ciprian@gmail.com>
2019-04-06 00:06:46 +0300

    Added a main form which links to 2 different scenaries

--- DBMS Lab2/Form1.Designer.cs
+++ DBMS Lab2/Form1.Designer.cs
@@ -28,13 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(26, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1195, 327);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Services:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(544, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 22);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Address:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(544, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 22);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(544, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 22);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ServiceID:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(710, 135);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(355, 29);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(710, 88);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(355, 29);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(710, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(355, 29);
+            this.textBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 22;
+            this.listBox1.Location = new System.Drawing.Point(29, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(387, 268);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(26, 379);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1194, 279);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(710, 185);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(355, 29);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(544, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 22);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Service_TicketID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1258, 669);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Book Antiqua", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DBMS Lab2 - Master-Detail Windows Form";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- DBMS Lab2/Form1.cs
+++ DBMS Lab2/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,87 @@ namespace DBMS_Lab2
         public Form1()
         {
             InitializeComponent();
+            this.Text = SqlConn.myApp();
+        }
+
+        private void ShowItems()
+        {
+            if(SqlConn.dataSet.Tables["Mechanics"] != null)
+            {
+                SqlConn.dataSet.Tables["Mechanics"].Clear();
+            }
+
+            SqlConn.sqlQuery =  "SELECT * " +
+                                "FROM Mechanic " +
+                                "WHERE ServiceID = " +
+                                "'" + textBox1.Text + "'";
+            SqlConn.command.CommandText = SqlConn.sqlQuery;
+            SqlConn.dataAdapter.SelectCommand = SqlConn.command;
+            SqlConn.dataAdapter.Fill(SqlConn.dataSet, "Mechanics");
+
+            dataGridView1.DataSource = SqlConn.dataSet.Tables["Mechanics"];
+
+            dataGridView1.Columns[0].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConn.sqlQuery = "SELECT * FROM Service";
+            SqlConn.OpenConn();
+            SqlConn.command = new SqlCommand(SqlConn.sqlQuery, SqlConn.connection);
+
+            SqlConn.dataAdapter = new SqlDataAdapter(SqlConn.command);
+            SqlConn.dataSet = new DataSet();
+            SqlConn.bindingSource = new BindingSource();
+            SqlConn.dataAdapter.Fill(SqlConn.dataSet, "Services");
+
+            foreach (DataRow r in SqlConn.dataSet.Tables["Services"].Rows)
+            {
+                listBox1.Items.Add(r["Name"].ToString());
+            }
+            listBox1.ValueMember = "ServiceID";
+
+            SqlConn.bindingSource.DataSource = SqlConn.dataSet.Tables["Services"];
+
+            textBox1.DataBindings.Add("Text", SqlConn.bindingSource, "ServiceID");
+            textBox2.DataBindings.Add("Text", SqlConn.bindingSource, "Name");
+            textBox3.DataBindings.Add("Text", SqlConn.bindingSource, "Address");
+            textBox4.DataBindings.Add("Text", SqlConn.bindingSource, "Service_TickID");
+
+            listBox1.SelectedIndex = 0;
+
+            var link = new DataGridViewLinkColumn();
+
+            link.DisplayIndex = 0;
+            link.DefaultCellStyle.NullValue = "SELECT";
+            link.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            link.Width = 60;
+
+            link.ActiveLinkColor = Color.White;
+            link.LinkBehavior = LinkBehavior.HoverUnderline;
+            link.LinkColor = Color.Crimson;
+            link.TrackVisitedState = true;
+            link.VisitedLinkColor = Color.YellowGreen;
+
+            dataGridView1.Columns.Add(link);
+            dataGridView1.AllowUserToAddRows = false;
+
+            ShowItems();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (SqlConn.bindingSource != null)
+            {
+                SqlConn.bindingSource.Position = listBox1.SelectedIndex;
+            }
+            ShowItems();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SqlConn.CloseConn();
         }
     }
 }
